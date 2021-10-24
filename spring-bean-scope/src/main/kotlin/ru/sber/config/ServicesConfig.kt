1204d60bd2ad8bff0dfd56130a8d7f71d75ce655
Kaaterinka <katerinabolotina@inbox.ru>
2021-10-24 19:09:43 +0300

    Bean-scope done

--- spring-bean-scope/src/main/kotlin/ru/sber/config/ServicesConfig.kt
+++ spring-bean-scope/src/main/kotlin/ru/sber/config/ServicesConfig.kt
@@ -1,7 +1,10 @@
 package ru.sber.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import ru.sber.services.PrototypeService
+import ru.sber.services.SingletonService
 
 @Configuration
 @ComponentScan("ru.sber.services")
